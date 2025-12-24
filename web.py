80d6b07598998415e53b7426c5b91ab91ea6022a
DotcubeTitan14 <jhajhariaananndi100@gmail.com>
2025-12-24 23:37:57 +0530

    change in logo

--- web.py
+++ web.py
@@ -3,29 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     if request.method == "POST":
         name = request.form.get("name")
-        email = request.form.get("email")
-        age = request.form.get("age")
-        student_class = request.form.get("class")
-        gender = request.form.get("gender")
-        difficulty = request.form.get("difficulty")
-        interest = request.form.get("interest")
+        return f"Hello {name}, your data received successfully!"
 
-        plan = f"""
-        Hello {name},
-
-        Based on your interest in {interest} and difficulty level {difficulty},
-        we recommend daily practice, concept learning, and weekly revisions.
-
-        Keep learning with GrowIQ!
-        """
-
-        return render_template("index.html", result=plan)
-
-    return render_template("index.html", result=None)
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
