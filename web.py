1b616a9104220014ba404b5f44dd9af84bbcb155
DotcubeTitan14 <jhajhariaananndi100@gmail.com>
2025-12-24 20:39:38 +0530

    add new codes

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    name = request.form.get("name")
+    return f"<h1>Hello {name}, your data was received successfully!</h1>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
